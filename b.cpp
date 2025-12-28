15c5347b0ec47ee295605766d248bdd20a4d1f57
ovinenterprisesmain-cell <ovinenterprises.main@gmail.com>
2025-12-28 18:44:32 +0530

    git push

---
+++ b.cpp
@@ -0,0 +1,10 @@
+//This code shows that how can we use cout keyword without using a namespace and we can also print in new line without using endl
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"akshit\n";
+    std::cout<<"bidang";
+    return 0;
+}
